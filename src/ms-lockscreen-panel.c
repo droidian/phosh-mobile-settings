7721528b1aa60f224e9ae8051f8dd5c729803adf
Guido Günther <agx@sigxcpu.org>
2022-08-26 21:52:29 +0200

    Allow to enable and disable lockscreen plugins
    Closes: #12

--- src/ms-lockscreen-panel.c
+++ src/ms-lockscreen-panel.c
@@ -10,6 +10,7 @@
 
 #include "mobile-settings-config.h"
 #include "ms-lockscreen-panel.h"
+#include "ms-plugin-row.h"
 
 #include <gio/gdesktopappinfo.h>
 #include <glib/gi18n.h>
@@ -18,14 +19,140 @@
 #define LOCKSCREEN_SCHEMA_ID "sm.puri.phosh.lockscreen"
 #define LOCKSCREEN_KEY_SCALE_TO_FIT "shuffle-keypad"
 
-struct _MsLockscreenPanel {
-  AdwBin     parent;
+#define LOCKSCREEN_PLUGINS_SCHEMA_ID "sm.puri.phosh.plugins"
+#define LOCKSCREEN_PLUGINS_KEY "lock-screen"
 
-  GSettings *settings;
-  GtkWidget *shuffle_switch;
+#define PLUGIN_PREFIX "libphosh-plugin-"
+#define PLUGIN_SUFFIX ".so"
+
+struct _MsLockscreenPanel {
+  AdwBin      parent;
+
+  GSettings  *settings;
+  GtkWidget  *shuffle_switch;
+
+  GSettings  *plugins_settings;
+  GtkListBox *plugins_listbox;
+  GListStore *plugins_store;
 };
 
 G_DEFINE_TYPE (MsLockscreenPanel, ms_lockscreen_panel, ADW_TYPE_BIN)
+
+
+static GStrv
+plugin_append (const char *const *plugins, const char *plugin)
+{
+  g_autoptr (GPtrArray) array = g_ptr_array_new_with_free_func (g_free);
+
+  for (int i = 0; i < g_strv_length ((GStrv)plugins); i++)
+    g_ptr_array_add (array, g_strdup (plugins[i]));
+
+  g_ptr_array_add (array, g_strdup (plugin));
+  g_ptr_array_add (array, NULL);
+
+  return (GStrv) g_ptr_array_steal (array, NULL);
+}
+
+
+static GStrv
+plugin_remove (const char *const *plugins, const char *plugin)
+{
+  g_autoptr (GPtrArray) array = g_ptr_array_new_with_free_func (g_free);
+
+  for (int i = 0; i < g_strv_length ((GStrv)plugins); i++) {
+    if (g_strcmp0 (plugins[i], plugin) == 0)
+      continue;
+    g_ptr_array_add (array, g_strdup (plugins[i]));
+  }
+  g_ptr_array_add (array, NULL);
+
+  return (GStrv) g_ptr_array_steal (array, NULL);
+}
+
+
+static void
+on_plugin_activated (MsLockscreenPanel *self, GParamSpec *pspec, MsPluginRow *row)
+{
+  gboolean enabled = ms_plugin_row_get_enabled (row);
+  const char *name = ms_plugin_row_get_name (row);
+
+  g_auto (GStrv) enabled_plugins = NULL;
+  g_auto (GStrv) e = NULL;
+
+  g_debug ("Plugin: %s: %d", name, enabled);
+  enabled_plugins = g_settings_get_strv (self->plugins_settings, LOCKSCREEN_PLUGINS_KEY);
+
+  if (enabled)
+    e = plugin_append ((const char * const *) enabled_plugins, name);
+  else
+    e = plugin_remove ((const char * const *) enabled_plugins, name);
+
+  g_settings_set_strv (self->plugins_settings, LOCKSCREEN_PLUGINS_KEY, (const char * const *)e);
+}
+
+
+static GtkWidget *
+create_plugins_row (gpointer object, gpointer user_data)
+{
+  return GTK_WIDGET (object);
+}
+
+
+static char *
+str_copy_part (const char *str, guint start, guint end)
+{
+  guint len;
+  char *out;
+
+  g_return_val_if_fail (str, NULL);
+  g_return_val_if_fail (end <= strlen (str), NULL);
+
+  len = strlen (str) - start - end;
+
+  out = g_new0 (char, len);
+  for (int i = 0; i < len; i++)
+    out[i] = str[start + i];
+
+  return out;
+}
+
+
+static void
+ms_lockscreen_panel_scan_phosh_lockscreen_plugins (MsLockscreenPanel *self)
+{
+  g_autoptr (GError) err = NULL;
+  g_autoptr (GDir) dir = g_dir_open (MOBILE_SETTINGS_PHOSH_PLUGINS_DIR, 0, &err);
+  const char *filename;
+  g_auto (GStrv) enabled_plugins = NULL;
+
+
+  if (dir == NULL) {
+    g_warning ("Failed to read phosh plugins from " MOBILE_SETTINGS_PHOSH_PLUGINS_DIR ": %s",
+               err->message);
+  }
+
+  enabled_plugins = g_settings_get_strv (self->plugins_settings, LOCKSCREEN_PLUGINS_KEY);
+  while ((filename = g_dir_read_name (dir))) {
+    GtkWidget *row;
+    g_autofree char *name = NULL;
+    gboolean enabled;
+
+    if (!g_str_has_prefix (filename, PLUGIN_PREFIX) || !g_str_has_suffix (filename, PLUGIN_SUFFIX))
+      continue;
+
+    name = str_copy_part (filename, strlen (PLUGIN_PREFIX), strlen (PLUGIN_SUFFIX));
+    enabled = g_strv_contains ((const gchar * const*)enabled_plugins, name);
+    g_debug ("Found plugin %s, name %s, enabled: %d", filename, name, enabled);
+    row = g_object_new (MS_TYPE_PLUGIN_ROW, "plugin-name", name, "enabled", enabled, NULL);
+    g_signal_connect_object (row,
+                             "notify::enabled",
+                             G_CALLBACK (on_plugin_activated),
+                             self,
+                             G_CONNECT_SWAPPED);
+
+    g_list_store_append (self->plugins_store, row);
+  }
+}
 
 static void
 ms_lockscreen_panel_finalize (GObject *object)
@@ -33,6 +160,8 @@ ms_lockscreen_panel_finalize (GObject *object)
   MsLockscreenPanel *self = MS_LOCKSCREEN_PANEL (object);
 
   g_clear_object (&self->settings);
+  g_clear_object (&self->plugins_settings);
+  g_clear_object (&self->plugins_store);
 
   G_OBJECT_CLASS (ms_lockscreen_panel_parent_class)->finalize (object);
 }
@@ -49,6 +178,7 @@ ms_lockscreen_panel_class_init (MsLockscreenPanelClass *klass)
   gtk_widget_class_set_template_from_resource (widget_class,
                                                "/org/sigxcpu/MobileSettings/ui/ms-lockscreen-panel.ui");
   gtk_widget_class_bind_template_child (widget_class, MsLockscreenPanel, shuffle_switch);
+  gtk_widget_class_bind_template_child (widget_class, MsLockscreenPanel, plugins_listbox);
 }
 
 
@@ -63,6 +193,14 @@ ms_lockscreen_panel_init (MsLockscreenPanel *self)
                    self->shuffle_switch,
                    "active",
                    G_SETTINGS_BIND_DEFAULT);
+
+  self->plugins_settings = g_settings_new (LOCKSCREEN_PLUGINS_SCHEMA_ID);
+  self->plugins_store = g_list_store_new (MS_TYPE_PLUGIN_ROW);
+  gtk_list_box_bind_model (self->plugins_listbox,
+                           G_LIST_MODEL (self->plugins_store),
+                           create_plugins_row,
+                           self, NULL);
+  ms_lockscreen_panel_scan_phosh_lockscreen_plugins (self);
 }
 
 
