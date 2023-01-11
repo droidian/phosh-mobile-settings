droidian/phosh-mobile-settings

commit 26b2f1c88f3dc9078d24df43167761dc1e01c632

lockscreen-panel: Allow plugins to have preferences

src/ms-lockscreen-panel.c
@@ -15,7 +15,9 @@
 #include <gio/gdesktopappinfo.h>
 #include <glib/gi18n.h>
 
-/* Verbatim from phosh */
+#include <phosh-plugin.h>
+
+/* Verbatim from phosh */ 
 #define LOCKSCREEN_SCHEMA_ID "sm.puri.phosh.lockscreen"
 #define LOCKSCREEN_KEY_SCALE_TO_FIT "shuffle-keypad"
 
@@ -34,9 +36,61 @@ struct _MsLockscreenPanel {
   GSettings  *plugins_settings;
   GtkListBox *plugins_listbox;
   GListStore *plugins_store;
+
+  GSimpleActionGroup *action_group;
 };
 
 G_DEFINE_TYPE (MsLockscreenPanel, ms_lockscreen_panel, ADW_TYPE_BIN)
+
+
+static AdwPreferencesWindow *
+load_prefs_window (const char *name)
+{
+  GIOExtensionPoint *ep;
+  GIOExtension *ext;
+  GType type;
+
+  ep = g_io_extension_point_lookup (PHOSH_PLUGIN_EXTENSION_POINT_LOCKSCREEN_WIDGET_PREFS);
+  g_return_val_if_fail (ep, NULL);
+
+  ext = g_io_extension_point_get_extension_by_name (ep, name);
+
+  g_debug ("Loading plugin %s", name);
+  type = g_io_extension_get_type (ext);
+  return g_object_new (type, NULL);
+}
+
+
+static void
+open_plugin_prefs_activated (GSimpleAction *action, GVariant *parameter, gpointer data)
+{
+  AdwPreferencesWindow *prefs;
+  GtkWindow*parent;
+  g_autoptr (GError) error = NULL;
+  g_autoptr (GKeyFile) keyfile = g_key_file_new ();
+  g_autofree char *name = NULL;
+  const char *filename;
+
+  g_variant_get (parameter, "&s", &filename);
+  g_assert (filename);
+  g_debug ("Prefs for'%s' activated", filename);
+
+  if (g_key_file_load_from_file (keyfile, filename, G_KEY_FILE_NONE, &error) == FALSE) {
+    g_warning ("Failed to load plugin info '%s': %s", filename, error->message);
+    return;
+  }
+
+  name = g_key_file_get_string (keyfile, "Prefs", "Id", NULL);
+  parent = gtk_application_get_active_window (
+    GTK_APPLICATION (g_application_get_default ()));
+  g_assert (parent);
+
+  prefs = load_prefs_window (name);
+  g_return_if_fail (prefs);
+
+  gtk_window_set_transient_for (GTK_WINDOW (prefs), parent);
+  gtk_window_present (GTK_WINDOW (prefs));
+}
 
 
 static GStrv
@@ -122,6 +176,7 @@ ms_lockscreen_panel_scan_phosh_lockscreen_plugins (MsLockscreenPanel *self)
     g_autofree char *description = NULL;
     g_autofree char *path = NULL;
     g_autofree char *plugin_path = NULL;
+    g_autofree char *prefs_path = NULL;
     g_autoptr (GError) error = NULL;
     g_autoptr (GKeyFile) keyfile = g_key_file_new ();
 
@@ -147,6 +202,8 @@ ms_lockscreen_panel_scan_phosh_lockscreen_plugins (MsLockscreenPanel *self)
       continue;
     }
 
+    prefs_path = g_key_file_get_string (keyfile, "Prefs", "Plugin", NULL);
+
     title = g_key_file_get_locale_string (keyfile, "Plugin", "Name", NULL, NULL);
     description = g_key_file_get_locale_string (keyfile, "Plugin", "Comment", NULL, NULL);
 
@@ -157,6 +214,8 @@ ms_lockscreen_panel_scan_phosh_lockscreen_plugins (MsLockscreenPanel *self)
                         "title", title,
                         "subtitle", description,
                         "enabled", enabled,
+                        "has-prefs", !!prefs_path,
+                        "filename", path,
                         NULL);
     g_signal_connect_object (row,
                              "notify::enabled",
@@ -176,6 +235,7 @@ ms_lockscreen_panel_finalize (GObject *object)
   g_clear_object (&self->settings);
   g_clear_object (&self->plugins_settings);
   g_clear_object (&self->plugins_store);
+  g_clear_object (&self->action_group);
 
   G_OBJECT_CLASS (ms_lockscreen_panel_parent_class)->finalize (object);
 }
@@ -196,9 +256,17 @@ ms_lockscreen_panel_class_init (MsLockscreenPanelClass *klass)
 }
 
 
+static GActionEntry entries[] =
+{
+  { .name = "open-plugin-prefs", .parameter_type = "s", .activate = open_plugin_prefs_activated },
+};
+
 static void
 ms_lockscreen_panel_init (MsLockscreenPanel *self)
 {
+  /* Scan prefs plugins */
+  g_io_modules_scan_all_in_directory (MOBILE_SETTINGS_PHOSH_PREFS_DIR);
+
   gtk_widget_init_template (GTK_WIDGET (self));
 
   self->settings = g_settings_new (LOCKSCREEN_SCHEMA_ID);
@@ -215,6 +283,16 @@ ms_lockscreen_panel_init (MsLockscreenPanel *self)
                            create_plugins_row,
                            self, NULL);
   ms_lockscreen_panel_scan_phosh_lockscreen_plugins (self);
+
+  self->action_group = g_simple_action_group_new ();
+  g_action_map_add_action_entries (G_ACTION_MAP (self->action_group),
+                                   entries,
+                                   G_N_ELEMENTS (entries),
+                                   self);
+  gtk_widget_insert_action_group (GTK_WIDGET (self),
+                                  "lockscreen-panel",
+                                  G_ACTION_GROUP (self->action_group));
+
 }
 
 

debian/control
@@ -10,6 +10,7 @@ Build-Depends:
  libgtk-4-dev,
  libsensors-dev,
  meson,
+ phosh-dev (>= 0.23.0),
 Standards-Version: 4.6.0
 Homepage: https://gitlab.gnome.org/guidog/phosh-mobile-settings/
 Rules-Requires-Root: no

meson.build
@@ -23,7 +23,7 @@ gmodule_dep = dependency('gmodule-2.0', version: glib_ver_cmp)
 gtk_dep = dependency('gtk4', version: '>= 4.4')
 gtk_wayland_dep = dependency('gtk4-wayland', version: '>= 4.4')
 adwaita_dep = dependency('libadwaita-1', version: '>= 1.2')
-phosh_plugins_dep = dependency('phosh-plugins', version: '>= 0.22.0')
+phosh_plugins_dep = dependency('phosh-plugins', version: '>= 0.23.0')
 wayland_client_dep = dependency('wayland-client', version: '>=1.14')
 wayland_protos_dep = dependency('wayland-protocols', version: '>=1.12')
 
@@ -92,7 +92,9 @@ config_h.set_quoted('MOBILE_SETTINGS_APP_ID', 'org.sigxcpu.MobileSettings')
 config_h.set_quoted('MOBILE_SETTINGS_VERSION', meson.project_version())
 config_h.set_quoted('MOBILE_SETTINGS_PLUGINS_DIR', ms_plugins_dir)
 config_h.set_quoted('MOBILE_SETTINGS_PHOSH_PLUGINS_DIR',
-		    phosh_plugins_dep.get_pkgconfig_variable('lockscreen_plugins_dir'))
+		    phosh_plugins_dep.get_variable(pkgconfig: 'lockscreen_plugins_dir'))
+config_h.set_quoted('MOBILE_SETTINGS_PHOSH_PREFS_DIR',
+		    phosh_plugins_dep.get_variable(pkgconfig: 'lockscreen_prefs_dir'))
 configure_file(
   output: 'mobile-settings-config.h',
   configuration: config_h,

src/ms-plugin-row.c
@@ -18,6 +18,8 @@ enum {
   PROP_0,
   PROP_PLUGIN_NAME,
   PROP_ENABLED,
+  PROP_FILENAME,
+  PROP_HAS_PREFS,
   PROP_LAST_PROP
 };
 static GParamSpec *props[PROP_LAST_PROP];
@@ -25,11 +27,31 @@ static GParamSpec *props[PROP_LAST_PROP];
 struct _MsPluginRow {
   AdwActionRow parent;
 
-  char        *name;
   GtkWidget   *toggle;
+  GtkWidget   *prefs;
+
+  char        *name;
+  char        *filename;
   gboolean     enabled;
+  gboolean     has_prefs;
+
+  GSimpleActionGroup *action_group;
 };
 G_DEFINE_TYPE (MsPluginRow, ms_plugin_row, ADW_TYPE_ACTION_ROW)
+
+
+static void
+open_prefs_activated (GSimpleAction *action,
+                      GVariant      *parameter,
+                      gpointer       data)
+{
+  MsPluginRow *self = MS_PLUGIN_ROW (data);
+
+  gtk_widget_activate_action (GTK_WIDGET (self),
+                              "lockscreen-panel.open-plugin-prefs",
+                              "s", self->name);
+}
+
 
 
 static void
@@ -45,8 +67,14 @@ ms_plugin_row_set_property (GObject      *object,
     self->name = g_value_dup_string (value);
     adw_preferences_row_set_title (ADW_PREFERENCES_ROW (self), self->name);
     break;
+  case PROP_FILENAME:
+    self->name = g_value_dup_string (value);
+    break;
   case PROP_ENABLED:
     self->enabled = g_value_get_boolean (value);
+    break;
+  case PROP_HAS_PREFS:
+    self->has_prefs = g_value_get_boolean (value);
     break;
   default:
     G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
@@ -67,13 +95,30 @@ ms_plugin_row_get_property (GObject    *object,
   case PROP_PLUGIN_NAME:
     g_value_set_string (value, ms_plugin_row_get_name (self));
     break;
+  case PROP_FILENAME:
+    g_value_set_string (value, self->filename);
+    break;
   case PROP_ENABLED:
     g_value_set_boolean (value, self->enabled);
+    break;
+  case PROP_HAS_PREFS:
+    g_value_set_boolean (value, self->has_prefs);
     break;
   default:
     G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
     break;
   }
+}
+
+
+static void
+ms_plugin_row_finalize (GObject *object)
+{
+  MsPluginRow *self = MS_PLUGIN_ROW (object);
+
+  g_clear_object (&self->action_group);
+
+  G_OBJECT_CLASS (ms_plugin_row_parent_class)->finalize (object);
 }
 
 
@@ -85,9 +130,19 @@ ms_plugin_row_class_init (MsPluginRowClass *klass)
 
   object_class->get_property = ms_plugin_row_get_property;
   object_class->set_property = ms_plugin_row_set_property;
+  object_class->finalize = ms_plugin_row_finalize;
 
   props[PROP_PLUGIN_NAME] =
     g_param_spec_string ("plugin-name", "", "",
+                         NULL,
+                         G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY);
+  /**
+   * MsPluginRow:filename:
+   *
+   * Name of the file with information about the plugin
+   */
+  props[PROP_FILENAME] =
+    g_param_spec_string ("filename", "", "",
                          NULL,
                          G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY);
 
@@ -95,18 +150,35 @@ ms_plugin_row_class_init (MsPluginRowClass *klass)
     g_param_spec_boolean ("enabled", "", "",
                           FALSE,
                           G_PARAM_READWRITE);
+  /**
+   * MsPluginRow:has-prefs:
+   *
+   * Whether the plugin has a preferences dialog
+   */
+  props[PROP_HAS_PREFS] =
+    g_param_spec_boolean ("has-prefs", "", "",
+                          FALSE,
+                          G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY);
 
   g_object_class_install_properties (object_class, PROP_LAST_PROP, props);
 
   gtk_widget_class_set_template_from_resource (widget_class,
                                                "/org/sigxcpu/MobileSettings/ui/ms-plugin-row.ui");
   gtk_widget_class_bind_template_child (widget_class, MsPluginRow, toggle);
+  gtk_widget_class_bind_template_child (widget_class, MsPluginRow, prefs);
 }
 
+
+static GActionEntry entries[] =
+{
+  { .name = "open-prefs", .activate = open_prefs_activated },
+};
 
 static void
 ms_plugin_row_init (MsPluginRow *self)
 {
+  GAction *action;
+
   gtk_widget_init_template (GTK_WIDGET (self));
 
   g_object_bind_property (self,
@@ -114,6 +186,20 @@ ms_plugin_row_init (MsPluginRow *self)
                           self->toggle,
                           "active",
                           G_BINDING_BIDIRECTIONAL | G_BINDING_SYNC_CREATE);
+
+  self->action_group = g_simple_action_group_new ();
+  g_action_map_add_action_entries (G_ACTION_MAP (self->action_group),
+                                   entries,
+                                   G_N_ELEMENTS (entries),
+                                   self);
+  gtk_widget_insert_action_group (GTK_WIDGET (self),
+                                  "row",
+                                  G_ACTION_GROUP (self->action_group));
+
+  action = g_action_map_lookup_action (G_ACTION_MAP (self->action_group), "open-prefs");
+  g_object_bind_property (self, "has-prefs",
+                          action, "enabled",
+                          G_BINDING_SYNC_CREATE);
 }
 
 

src/ui/ms-plugin-row.ui
@@ -8,5 +8,14 @@
         <property name="valign">center</property>
       </object>
     </child>
+    <child type="suffix">
+      <object class="GtkButton" id="prefs">
+        <property name="valign">center</property>
+        <property name="halign">center</property>
+        <property name="action-name">row.open-prefs</property>
+        <property name="icon-name">emblem-system-symbolic</property>
+        <style><class name="flat"/></style>
+      </object>
+    </child>
   </template>
 </interface>

commit 263a0f7b64a5c1974046def8d3c539f0b2e45e64

lockscreen-panels: Drop plugins that start with '/'
This clears out plugins that were accidentally added with the full path.

Fixes: 26b2f1c88f3dc9078d24df43167761dc1e01c632

src/ms-lockscreen-panel.c
@@ -93,13 +93,37 @@ open_plugin_prefs_activated (GSimpleAction *action, GVariant *parameter, gpointe
 }
 
 
+static gboolean
+is_plugin_name_valid (const char *name)
+{
+  if (name == NULL)
+    return FALSE;
+
+  if (name[0] == '\0')
+    return FALSE;
+
+  /* Absolute path is not allowed */
+  if (name[0] == '/')
+    return FALSE;
+
+  return TRUE;
+}
+
+
 static GStrv
 plugin_append (const char *const *plugins, const char *plugin)
 {
   g_autoptr (GPtrArray) array = g_ptr_array_new_with_free_func (g_free);
 
-  for (int i = 0; i < g_strv_length ((GStrv)plugins); i++)
-    g_ptr_array_add (array, g_strdup (plugins[i]));
+  for (int i = 0; i < g_strv_length ((GStrv)plugins); i++) {
+    const char *name = plugins[i];
+
+    if (!is_plugin_name_valid (name)) {
+      g_warning ("Plugin name '%s' invalid, dropping", name);
+      continue;
+    }
+    g_ptr_array_add (array, g_strdup (name));
+  }
 
   g_ptr_array_add (array, g_strdup (plugin));
   g_ptr_array_add (array, NULL);
@@ -114,8 +138,14 @@ plugin_remove (const char *const *plugins, const char *plugin)
   g_autoptr (GPtrArray) array = g_ptr_array_new_with_free_func (g_free);
 
   for (int i = 0; i < g_strv_length ((GStrv)plugins); i++) {
-    if (g_strcmp0 (plugins[i], plugin) == 0)
+    const char *name = plugins[i];
+
+    if (g_strcmp0 (name, plugin) == 0)
       continue;
+    if (!is_plugin_name_valid (name)) {
+      g_warning ("Plugin name '%s' invalid, dropping", name);
+      continue;
+    }
     g_ptr_array_add (array, g_strdup (plugins[i]));
   }
   g_ptr_array_add (array, NULL);