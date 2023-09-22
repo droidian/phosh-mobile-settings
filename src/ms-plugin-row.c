droidian/phosh-mobile-settings

commit b5ed64cc841d64be26731e6f4e39e091df28163b

lockscreen-panel: Allow to reorder the plugins
For that add a popover with "move up" and "move down" buttons to each plugin row.

closes: https://gitlab.gnome.org/guidog/phosh-mobile-settings/-/issues/17

src/ms-plugin-row.c
@@ -24,10 +24,18 @@ enum {
 };
 static GParamSpec *props[PROP_LAST_PROP];
 
+enum
+{
+  SIGNAL_MOVE_ROW,
+  SIGNAL_LAST
+};
+static guint signals[SIGNAL_LAST] = { 0, };
+
+
 struct _MsPluginRow {
   AdwActionRow parent;
 
-  GtkWidget   *toggle;
+  GtkSwitch   *toggle;
   GtkWidget   *prefs;
 
   char        *name;
@@ -112,6 +120,81 @@ ms_plugin_row_get_property (GObject    *object,
 
 
 static void
+update_move_actions_after_row_moved_up (MsPluginRow *self)
+{
+  GtkListBox *list_box = GTK_LIST_BOX (gtk_widget_get_parent (GTK_WIDGET (self)));
+  gint previous_idx = gtk_list_box_row_get_index (GTK_LIST_BOX_ROW (self)) - 1;
+  GtkListBoxRow *previous_row = gtk_list_box_get_row_at_index (list_box, previous_idx);
+
+  if (gtk_list_box_get_row_at_index (list_box, previous_idx - 1) == NULL) {
+    gtk_widget_action_set_enabled (GTK_WIDGET (self), "row.move-up", FALSE);
+  }
+
+  gtk_widget_action_set_enabled (GTK_WIDGET (previous_row), "row.move-up", TRUE);
+  gtk_widget_action_set_enabled (GTK_WIDGET (previous_row), "row.move-down", gtk_widget_get_next_sibling (GTK_WIDGET (self)) != NULL);
+  gtk_widget_action_set_enabled (GTK_WIDGET (self), "row.move-down", TRUE);
+}
+
+static void
+update_move_actions_after_row_moved_down (MsPluginRow *self)
+{
+  GtkListBox *list_box = GTK_LIST_BOX (gtk_widget_get_parent (GTK_WIDGET (self)));
+  gint next_idx = gtk_list_box_row_get_index (GTK_LIST_BOX_ROW (self)) + 1;
+  GtkListBoxRow *next_row = gtk_list_box_get_row_at_index (list_box, next_idx);
+
+  if (gtk_widget_get_next_sibling (GTK_WIDGET (next_row)) == NULL) {
+    gtk_widget_action_set_enabled (GTK_WIDGET (self), "row.move-down", FALSE);
+  }
+
+  gtk_widget_action_set_enabled (GTK_WIDGET (next_row), "row.move-up", next_idx-1 != 0);
+  gtk_widget_action_set_enabled (GTK_WIDGET (next_row), "row.move-down", TRUE);
+  gtk_widget_action_set_enabled (GTK_WIDGET (self), "row.move-up", TRUE);
+}
+
+static void
+move_up_activated (GSimpleAction *action,
+                   GVariant      *parameter,
+                   gpointer       user_data)
+{
+  MsPluginRow *self = MS_PLUGIN_ROW (user_data);
+  GtkListBox *list_box = GTK_LIST_BOX (gtk_widget_get_parent (GTK_WIDGET (self)));
+  gint previous_idx = gtk_list_box_row_get_index (GTK_LIST_BOX_ROW (self)) - 1;
+  GtkListBoxRow *previous_row = gtk_list_box_get_row_at_index (list_box, previous_idx);
+
+  if (previous_row == NULL)
+    return;
+
+  update_move_actions_after_row_moved_up (self);
+
+  g_signal_emit (self,
+                 signals[SIGNAL_MOVE_ROW],
+                 0,
+                 previous_row);
+}
+
+static void
+move_down_activated (GSimpleAction *action,
+                     GVariant      *parameter,
+                     gpointer       user_data)
+{
+  MsPluginRow *self = MS_PLUGIN_ROW (user_data);
+  GtkListBox *list_box = GTK_LIST_BOX (gtk_widget_get_parent (GTK_WIDGET (self)));
+  gint next_idx = gtk_list_box_row_get_index (GTK_LIST_BOX_ROW (self)) + 1;
+  GtkListBoxRow *next_row = gtk_list_box_get_row_at_index (list_box, next_idx);
+
+  if (next_row == NULL)
+    return;
+
+  update_move_actions_after_row_moved_down (self);
+
+  g_signal_emit (next_row,
+                 signals[SIGNAL_MOVE_ROW],
+                 0,
+                 self);
+}
+
+
+static void
 ms_plugin_row_finalize (GObject *object)
 {
   MsPluginRow *self = MS_PLUGIN_ROW (object);
@@ -166,12 +249,24 @@ ms_plugin_row_class_init (MsPluginRowClass *klass)
                                                "/org/sigxcpu/MobileSettings/ui/ms-plugin-row.ui");
   gtk_widget_class_bind_template_child (widget_class, MsPluginRow, toggle);
   gtk_widget_class_bind_template_child (widget_class, MsPluginRow, prefs);
+
+  signals[SIGNAL_MOVE_ROW] =
+    g_signal_new ("move-row",
+                  G_TYPE_FROM_CLASS (object_class),
+                  G_SIGNAL_RUN_LAST,
+                  0,
+                  NULL, NULL,
+                  NULL,
+                  G_TYPE_NONE,
+                  1, MS_TYPE_PLUGIN_ROW);
 }
 
 
 static GActionEntry entries[] =
 {
   { .name = "open-prefs", .activate = open_prefs_activated },
+  { "move-up", move_up_activated, NULL, NULL, NULL, { 0 }  },
+  { "move-down", move_down_activated, NULL, NULL, NULL, { 0 } }
 };
 
 static void
@@ -197,9 +292,14 @@ ms_plugin_row_init (MsPluginRow *self)
                                   G_ACTION_GROUP (self->action_group));
 
   action = g_action_map_lookup_action (G_ACTION_MAP (self->action_group), "open-prefs");
-  g_object_bind_property (self, "has-prefs",
-                          action, "enabled",
+  g_object_bind_property (self, "has-prefs", action, "enabled",
                           G_BINDING_SYNC_CREATE);
+
+  g_action_map_add_action_entries (G_ACTION_MAP (self->action_group),
+                                   entries,
+                                   G_N_ELEMENTS (entries),
+                                   self);
+  gtk_widget_insert_action_group (GTK_WIDGET (self), "row", G_ACTION_GROUP (self->action_group));
 }
 
 

src/ms-lockscreen-panel.c
@@ -37,6 +37,8 @@ struct _MsLockscreenPanel {
   GtkListBox *plugins_listbox;
   GListStore *plugins_store;
 
+  MsPluginRow *selected_row;
+
   GSimpleActionGroup *action_group;
 };
 
@@ -111,7 +113,7 @@ is_plugin_name_valid (const char *name)
 
 
 static void
-on_plugin_activated (MsLockscreenPanel *self, GParamSpec *pspec, MsPluginRow *row)
+save_plugin_store (MsLockscreenPanel *self)
 {
   g_auto (GStrv) ret = NULL;
   g_autoptr (GStrvBuilder) builder = g_strv_builder_new ();
@@ -119,7 +121,7 @@ on_plugin_activated (MsLockscreenPanel *self, GParamSpec *pspec, MsPluginRow *ro
   guint n_plugins = g_list_model_get_n_items (G_LIST_MODEL (self->plugins_store));
 
   for (guint i = 0; i < n_plugins; i++) {
-    MsPluginRow *plugin_row = MS_PLUGIN_ROW (g_list_model_get_item (G_LIST_MODEL (self->plugins_store), i));
+    MsPluginRow *plugin_row = g_list_model_get_item (G_LIST_MODEL (self->plugins_store), i);
     gboolean enabled = ms_plugin_row_get_enabled (plugin_row);
     const char *name = ms_plugin_row_get_name (plugin_row);
 
@@ -135,6 +137,71 @@ on_plugin_activated (MsLockscreenPanel *self, GParamSpec *pspec, MsPluginRow *ro
   }
   ret = g_strv_builder_end (builder);
   g_settings_set_strv (self->plugins_settings, LOCKSCREEN_PLUGINS_KEY, (const gchar * const *)ret);
+}
+
+
+static void
+lockscreen_panel_update_enabled_move_actions (MsLockscreenPanel *self)
+{
+  GtkWidget *child;
+
+  for (child = gtk_widget_get_first_child (GTK_WIDGET (self->plugins_listbox));
+       child != NULL;
+       child = gtk_widget_get_next_sibling (child)) {
+        gint row_idx;
+
+        if (!MS_IS_PLUGIN_ROW (child))
+          continue;
+
+        row_idx = gtk_list_box_row_get_index (GTK_LIST_BOX_ROW (child));
+        gtk_widget_action_set_enabled (GTK_WIDGET (child), "row.move-up", row_idx != 0);
+        gtk_widget_action_set_enabled (GTK_WIDGET (child), "row.move-down",
+                                       gtk_widget_get_next_sibling (GTK_WIDGET (child)) != NULL);
+       }
+}
+
+
+static void
+lockscreen_panel_move_selected (MsLockscreenPanel *self,
+                                gboolean           down)
+{
+  gint selected_idx, dest_idx;
+  MsPluginRow *plugin_row = NULL;
+
+  selected_idx = gtk_list_box_row_get_index (GTK_LIST_BOX_ROW (self->selected_row));
+  dest_idx = down ? selected_idx + 1 : selected_idx - 1;
+
+  plugin_row = g_list_model_get_item (G_LIST_MODEL (self->plugins_store), selected_idx);
+  g_list_store_remove (self->plugins_store, selected_idx);
+  g_list_store_insert (self->plugins_store, dest_idx, plugin_row);
+
+  lockscreen_panel_update_enabled_move_actions (self);
+}
+
+
+static void
+on_row_moved (MsLockscreenPanel  *self,
+              MsPluginRow        *dest_row,
+              MsPluginRow        *row)
+{
+  gint source_idx = gtk_list_box_row_get_index (GTK_LIST_BOX_ROW (row));
+  gint dest_idx = gtk_list_box_row_get_index (GTK_LIST_BOX_ROW (dest_row));
+  gboolean down;
+
+  self->selected_row = row;
+
+  down = (source_idx - dest_idx) < 0;
+  for (int i = 0; i < ABS (source_idx - dest_idx); i++)
+    lockscreen_panel_move_selected (self, down);
+
+  save_plugin_store (self);
+}
+
+
+static void
+on_plugin_activated (MsLockscreenPanel *self, GParamSpec *pspec, MsPluginRow *row)
+{
+  save_plugin_store (self);
 }
 
 
@@ -237,6 +304,10 @@ ms_lockscreen_panel_scan_phosh_lockscreen_plugins (MsLockscreenPanel *self)
                              G_CONNECT_SWAPPED);
 
     g_list_store_append (self->plugins_store, row);
+
+    g_signal_connect_object (row, "move-row",
+                             G_CALLBACK (on_row_moved), self,
+                             G_CONNECT_SWAPPED);
   }
   sort_plugins_store (self);
 }

src/ui/ms-plugin-row.ui
@@ -3,7 +3,7 @@
   <requires lib="gtk" version="4.0"/>
   <template class="MsPluginRow" parent="AdwActionRow">
     <property name="activatable-widget">toggle</property>
-    <child>
+    <child type="suffix">
       <object class="GtkSwitch" id="toggle">
         <property name="valign">center</property>
       </object>
@@ -17,5 +17,39 @@
         <style><class name="flat"/></style>
       </object>
     </child>
+    <child type="suffix">
+      <object class="GtkSeparator">
+        <property name="orientation">vertical</property>
+        <property name="margin-top">12</property>
+        <property name="margin-bottom">12</property>
+      </object>
+    </child>
+    <child type="suffix">
+      <object class="GtkMenuButton">
+        <property name="valign">center</property>
+        <property name="icon-name">view-more-symbolic</property>
+        <property name="menu-model">move_row_menu</property>
+        <accessibility>
+          <property name="label" translatable="yes">More options…</property>
+        </accessibility>
+        <style>
+          <class name="flat"/>
+        </style>
+      </object>
+    </child>
   </template>
+
+  <!-- Move Row Menu -->
+  <menu id="move_row_menu">
+    <section>
+      <item>
+        <attribute name="label" translatable="yes">Move Up</attribute>
+        <attribute name="action">row.move-up</attribute>
+      </item>
+      <item>
+        <attribute name="label" translatable="yes">Move Down</attribute>
+        <attribute name="action">row.move-down</attribute>
+      </item>
+    </section>
+  </menu>
 </interface>

commit 87eefe37991032875510ec0daffc4647d99fa88f

plugin-row: Don't insert actions twice
This unbreaks setting the plugin prefs

Fixes: b5ed64cc841d64be26731e6f4e39e091df28163b

Signed-off-by: Guido Günther <agx@sigxcpu.org>

src/ms-plugin-row.c
@@ -297,12 +297,6 @@ ms_plugin_row_init (MsPluginRow *self)
 
   action = g_action_map_lookup_action (G_ACTION_MAP (self->action_group), "open-prefs");
   g_object_bind_property (self, "has-prefs", action, "enabled", G_BINDING_SYNC_CREATE);
-
-  g_action_map_add_action_entries (G_ACTION_MAP (self->action_group),
-                                   entries,
-                                   G_N_ELEMENTS (entries),
-                                   self);
-  gtk_widget_insert_action_group (GTK_WIDGET (self), "row", G_ACTION_GROUP (self->action_group));
 }
 
 