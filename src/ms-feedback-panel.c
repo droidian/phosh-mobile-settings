droidian/phosh-mobile-settings

commit 73507d0f82d2f431898e12fe91db3821b3c07cec

treewide: Move to mobi.phosh prefix
Signed-off-by: Guido Günther <agx@sigxcpu.org>

src/ms-feedback-panel.c
@@ -23,7 +23,7 @@
 #define FEEDBACKD_KEY_PROFILE "profile"
 #define FEEDBACKD_THEME_VAR "FEEDBACK_THEME"
 #define APP_SCHEMA FEEDBACKD_SCHEMA_ID ".application"
-#define APP_PREFIX "/org/sigxcpu/feedbackd/application/"
+#define APP_PREFIX "/mobi/phosh/feedbackd/application/"
 
 #define NOTIFICATIONS_SCHEMA "sm.puri.phosh.notifications"
 #define NOTIFICATIONS_URGENCY_ENUM "sm.puri.phosh.NotificationUrgency"
@@ -487,7 +487,7 @@ ms_feedback_panel_class_init (MsFeedbackPanelClass *klass)
 
   g_type_ensure (MS_TYPE_FEEDBACK_PROFILE);
   gtk_widget_class_set_template_from_resource (widget_class,
-                                               "/org/sigxcpu/MobileSettings/ui/ms-feedback-panel.ui");
+                                               "/mobi/phosh/MobileSettings/ui/ms-feedback-panel.ui");
   gtk_widget_class_bind_template_child (widget_class, MsFeedbackPanel, app_listbox);
   gtk_widget_class_bind_template_child (widget_class, MsFeedbackPanel, toast_overlay);
   gtk_widget_class_bind_template_callback (widget_class, item_feedback_profile_name);

data/icons/meson.build
@@ -1,4 +1,4 @@
-application_id = 'org.sigxcpu.MobileSettings'
+application_id = 'mobi.phosh.MobileSettings'
 
 scalable_dir = join_paths('hicolor', 'scalable', 'apps')
 install_data(

data/meson.build
@@ -1,6 +1,6 @@
 desktop_file = i18n.merge_file(
-  input: 'org.sigxcpu.MobileSettings.desktop.in',
-  output: 'org.sigxcpu.MobileSettings.desktop',
+  input: 'mobi.phosh.MobileSettings.desktop.in',
+  output: 'mobi.phosh.MobileSettings.desktop',
   type: 'desktop',
   po_dir: '../po',
   install: true,
@@ -8,8 +8,8 @@ desktop_file = i18n.merge_file(
 )
 
 appstream_file = i18n.merge_file(
-  input: 'org.sigxcpu.MobileSettings.metainfo.xml.in',
-  output: 'org.sigxcpu.MobileSettings.metainfo.xml',
+  input: 'mobi.phosh.MobileSettings.metainfo.xml.in',
+  output: 'mobi.phosh.MobileSettings.metainfo.xml',
   po_dir: '../po',
   install: true,
   install_dir: join_paths(get_option('datadir'), 'metainfo')

data/mobi.phosh.MobileSettings.desktop.in
@@ -2,7 +2,7 @@
 Name=Mobile Settings
 Exec=phosh-mobile-settings
 # Translators: Do NOT translate or transliterate this text (this is an icon file name)
-Icon=org.sigxcpu.MobileSettings
+Icon=mobi.phosh.MobileSettings
 Terminal=false
 Type=Application
 Categories=GNOME;GTK;Settings;

data/mobi.phosh.MobileSettings.metainfo.xml.in
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <component type="desktop">
-  <id>org.sigxcpu.MobileSettings.desktop</id>
+  <id>mobi.phosh.MobileSettings.desktop</id>
   <name>Phosh Mobile Settings</name>
   <metadata_license>CC0-1.0</metadata_license>
   <project_license>GPL-3.0-or-later</project_license>
@@ -26,7 +26,7 @@
   <url type="homepage">https://gitlab.gnome.org//guidog/phosh-mobile-settings</url>
   <url type="bugtracker">https://gitlab.gnome.org//guidog/phosh-mobile-settings/issues</url>
 
-  <launchable type="desktop-id">org.sigxcpu.MobileSettings.desktop</launchable>
+  <launchable type="desktop-id">mobi.phosh.MobileSettings.desktop</launchable>
 
   <screenshots>
     <screenshot type="default">

debian/phosh-mobile-settings.install
@@ -1,5 +1,5 @@
 usr/bin/phosh-mobile-settings
-usr/share/applications/org.sigxcpu.MobileSettings.desktop
-usr/share/icons/hicolor/scalable/apps/org.sigxcpu.MobileSettings.svg
-usr/share/icons/hicolor/symbolic/apps/org.sigxcpu.MobileSettings-symbolic.svg
-usr/share/metainfo/org.sigxcpu.MobileSettings.appdata.xml
+usr/share/applications/mobi.phosh.MobileSettings.desktop
+usr/share/icons/hicolor/scalable/apps/mobi.phosh.MobileSettings.svg
+usr/share/icons/hicolor/symbolic/apps/mobi.phosh.MobileSettings-symbolic.svg
+usr/share/metainfo/mobi.phosh.MobileSettings.appdata.xml

commit b6fdc1a4d1dc0cdf5217d9ba0dae4b3d6eb776ca

feedback-panel: Use correct schema id for feedbackd
Fixes: 73507d0 ("treewide: Move to mobi.phosh prefix")
Tested-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phosh-mobile-settings/-/merge_requests/99>

src/ms-feedback-panel.c
@@ -23,7 +23,7 @@
 #define FEEDBACKD_KEY_PROFILE "profile"
 #define FEEDBACKD_THEME_VAR "FEEDBACK_THEME"
 #define APP_SCHEMA FEEDBACKD_SCHEMA_ID ".application"
-#define APP_PREFIX "/mobi/phosh/feedbackd/application/"
+#define APP_PREFIX "/org/sigxcpu/feedbackd/application/"
 
 #define NOTIFICATIONS_SCHEMA "sm.puri.phosh.notifications"
 #define NOTIFICATIONS_URGENCY_ENUM "sm.puri.phosh.NotificationUrgency"