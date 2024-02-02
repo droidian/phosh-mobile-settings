droidian/phosh-mobile-settings

commit 6f402147901a759a81278407947e9c594dd83ceb

application: Override handle_local_options vfunc
This will allow for handling some options locally
(e.g., --version) efficiently by registering
array of accepted options.

Part-of: <https://gitlab.gnome.org/World/Phosh/phosh-mobile-settings/-/merge_requests/89>

src/mobile-settings-application.c
@@ -54,6 +54,11 @@ struct _MobileSettingsApplication {
 G_DEFINE_TYPE (MobileSettingsApplication, mobile_settings_application, ADW_TYPE_APPLICATION)
 
 
+static const GOptionEntry entries[] = {
+  G_OPTION_ENTRY_NULL
+};
+
+
 static void
 mobile_settings_application_get_property (GObject    *object,
                                           guint       property_id,
@@ -145,6 +150,16 @@ mobile_settings_application_new (gchar *application_id)
                        NULL);
 }
 
+
+static int
+mobile_settings_application_handle_local_options (GApplication *self,
+                                                  GVariantDict *options)
+{
+  return G_APPLICATION_CLASS (mobile_settings_application_parent_class)->handle_local_options (self,
+                                                                                               options);
+}
+
+
 static void
 mobile_settings_application_activate (GApplication *app)
 {
@@ -193,6 +208,7 @@ mobile_settings_application_class_init (MobileSettingsApplicationClass *klass)
   object_class->get_property = mobile_settings_application_get_property;
 
   app_class->activate = mobile_settings_application_activate;
+  app_class->handle_local_options = mobile_settings_application_handle_local_options;
 
   props[PROP_TOPLEVEL_TRACKER] =
     g_param_spec_object ("toplevel-tracker", "", "",
@@ -258,6 +274,8 @@ mobile_settings_application_init (MobileSettingsApplication *self)
     "<primary>q",
     NULL,
   });
+
+  g_application_add_main_option_entries (G_APPLICATION (self), entries);
 
   self->device_plugin_loader = ms_plugin_loader_new (plugin_dirs, MS_EXTENSION_POINT_DEVICE_PANEL);
   self->wayland_protocols = g_hash_table_new_full (g_str_hash,

commit 7079d9f4be2ebbe0677e1df75c50ddf50284a1b0

application: Don't use self for GApplication
self refers to an object of type PhoshMobileSettingsApplication not
GApplication. Adjust the variable names in functions that did this
differently. This makes it obvious where casts are needed.

While at that fix an overly long line by introducing a variable.

Fixes: 6f40214 ("application: Override handle_local_options vfunc") and
       related commits.

Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phosh-mobile-settings/-/merge_requests/109>

src/mobile-settings-application.c
@@ -151,7 +151,7 @@ get_active_window (MobileSettingsApplication *self)
 
 
 static void
-list_available_panels (GApplication *self)
+list_available_panels (GApplication *app)
 {
   MobileSettingsWindow *window;
   g_autoptr (GListModel) list = NULL;
@@ -208,13 +208,14 @@ mobile_settings_application_new (gchar *application_id)
 
 
 static int
-mobile_settings_application_handle_local_options (GApplication *self,
+mobile_settings_application_handle_local_options (GApplication *app,
                                                   GVariantDict *options)
 {
   g_autofree GStrv panels = NULL;
+  GApplicationClass *app_class = G_APPLICATION_CLASS (mobile_settings_application_parent_class);
 
   if (g_variant_dict_contains (options, "list")) {
-    list_available_panels (self);
+    list_available_panels (app);
 
     return 0;
   } else if (g_variant_dict_lookup (options, G_OPTION_REMAINING, "^a&ay", &panels)) {
@@ -223,12 +224,11 @@ mobile_settings_application_handle_local_options (GApplication *self,
     g_return_val_if_fail (panels && panels[0], EXIT_FAILURE);
     panel = panels[0];
 
-    g_application_register (G_APPLICATION (self), NULL, NULL);
-    g_action_group_activate_action (G_ACTION_GROUP (self), "set-panel", g_variant_new ("(s)", panel));
+    g_application_register (G_APPLICATION (app), NULL, NULL);
+    g_action_group_activate_action (G_ACTION_GROUP (app), "set-panel", g_variant_new ("(s)", panel));
   }
 
-  return G_APPLICATION_CLASS (mobile_settings_application_parent_class)->handle_local_options (self,
-                                                                                               options);
+  return app_class->handle_local_options (app, options);
 }
 
 