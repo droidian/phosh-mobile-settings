c60c95581a261cc546bbd9ea3c1560957e8b586d
Guido Günther <agx@sigxcpu.org>
2024-08-13 18:24:46 +0200

    feedback-panel: Use phosh's enums for screen wakup flags
    Signed-off-by: Guido Günther <agx@sigxcpu.org>
    Closes: https://gitlab.gnome.org/World/Phosh/phosh-mobile-settings/-/issues/55
    Part-of: <https://gitlab.gnome.org/World/Phosh/phosh-mobile-settings/-/merge_requests/148>

--- src/mobile-settings-enums.h
+++ src/mobile-settings-enums.h
@@ -31,20 +31,4 @@ typedef enum {
   MS_PHOSH_NOTIFICATION_URGENCY_CRITICAL,
 } MsPhoshNotificationUrgency;
 
-/**
- * MsPhoshNotifyScreenWakeupFlags:
- * @MS_PHOSH_NOTIFY_SCREEN_WAKEUP_FLAG_NONE: No wakeup
- * @MS_PHOSH_NOTIFY_SCREEN_WAKEUP_FLAG_URGENCY: Wakeup screen based on notification urgency
- * @MS_PHOSH_NOTIFY_SCREEN_WAKEUP_FLAG_CATEGORY: Wakeup screen based on notification category
- * @MS_PHOSH_NOTIFY_SCREEN_WAKEUP_FLAG_ANY: Wakeup screen on any notification
- *
- * What notification properties trigger screen wakeup
- */
-typedef enum {
-  MS_PHOSH_NOTIFY_SCREEN_WAKEUP_FLAG_NONE     = 0, /*< skip >*/
-  MS_PHOSH_NOTIFY_SCREEN_WAKEUP_FLAG_ANY      = (1 << 0),
-  MS_PHOSH_NOTIFY_SCREEN_WAKEUP_FLAG_URGENCY  = (1 << 1),
-  MS_PHOSH_NOTIFY_SCREEN_WAKEUP_FLAG_CATEGORY = (1 << 2),
-} MsPhoshNotifyScreenWakeupFlags;
-
 G_END_DECLS
